f9deb5e4c4c89afd52552e64de588ee5156953dc
drommerkode <drommerkode@gmail.com>
2024-08-10 13:32:21 +0300

    Cur world type

--- DragAndDropM3/Assets/Scripts/LevelLoader.cs
+++ DragAndDropM3/Assets/Scripts/LevelLoader.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelLoader : MonoBehaviour
@@ -6,11 +7,20 @@ public class LevelLoader : MonoBehaviour
     [SerializeField] private int levelNum;
     [SerializeField] private int itemsVariants = 10;
     [SerializeField] private int levelType;
-    [SerializeField] private GameObject level;
+    [SerializeField] private List<GameObject> level = new List<GameObject>();
+    [SerializeField] private int levelCountsPerWorld = 5;
 
     public void Init(ManagerUI _managerUI) {
+
+        int openedLevel = SaveLoad.saveData.levelsOpened;
+        int curLevelMenuType = (openedLevel - 1) / levelCountsPerWorld;
+
+        //int levelMenuType = curLevelMenuType + i - 1;
+        int levelMenuTypeDiv = curLevelMenuType / level.Count;
+        int levelMenuTypeAfterCycle = curLevelMenuType - levelMenuTypeDiv * level.Count;
+
         levelNum = ManagerGame.instance.GetCurLevel();
-        GameObject l = Instantiate(level);
+        GameObject l = Instantiate(level[levelMenuTypeAfterCycle]);
         ManagerLevel mi = l.GetComponent<ManagerLevel>();
         mi.Init(itemsVariants, _managerUI);
         //ManagerGame.instance.LevelLoaded();
